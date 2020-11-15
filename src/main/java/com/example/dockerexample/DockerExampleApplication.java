62cf47953cb8cce6116d2ab3f0492c2786ac6377
MINHAJUL QURAISHI <mquraishi@MINHAJULs-MacBook-Pro-2.local>
2020-11-15 10:34:35 -0500

    first commit

---
+++ src/main/java/com/example/dockerexample/DockerExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.dockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DockerExampleApplication.class, args);
+	}
+
+}
